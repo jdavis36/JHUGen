b3f011fde2632bf25ae0f6b6fd5b09cf6a155842
Mohit Srivastav <vir.moh@gmail.com>
2026-06-03 14:43:58 -0400

    changes to Ztt and Lexicon

--- MCFM-JHUGen/src/Inc/AnomZffCouplings.f
+++ MCFM-JHUGen/src/Inc/AnomZffCouplings.f
@@ -2,11 +2,12 @@
 
         double precision reZ,leZ,lnZ,rnZ
         double precision rquZ,lquZ,rqdZ,lqdZ 
-        double precision clanou,cranou,clanod,cranod
+        double precision clanou,cranou,clanod,cranod,clanot,cranot
 
         common/AnomZffCouplings/
      & AllowAnomalousZffCouplings,
 
      & reZ,leZ,lnZ,rnZ,
      & rquZ,lquZ,rqdZ,lqdZ,
-     & clanou,cranou,clanod,cranod
+     & clanou,cranou,clanod,cranod,
+     & clanot,cranot
